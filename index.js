dcb2b0be922fea963a7702e5c6b7052714f4deab
Mari Topel <maritopel@gmail.com>
2025-05-03 19:07:06 -0500

    did the assigned coursework

---
+++ index.js
@@ -0,0 +1,21 @@
+var code1;
+
+code1 = 7 + 3;
+
+var code2 = 50 - 100 / 10;
+
+var code3 = 38 + (3 % 2);
+
+var message = "The vault has been secured. The combination is:";
+
+var codeA = code1 + "-" + code2 + "-" + code3;
+
+var codeB = `${code1}-${code2}-${code3}`;
+
+/* asked in class what the correct format for answering question 8. And was advised that 
+should have a console.log for each individual message, codeA, and codeB. Not have them 
+together in one message. */
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
